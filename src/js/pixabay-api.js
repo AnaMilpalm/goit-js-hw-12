a701bec7a0a5d5bbae7ceac351aec5778297dd93
Anastacy Slyusar <anastacy764@gmail.com>
2024-10-01 18:18:38 +0200

    hw 12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,16 @@
-
+import axios from "axios";
 const API_KEY = '46203221-a91cc8cf493e831e00cc5b047';
-
 const BASE_URL = 'https://pixabay.com/api/';
         
-export const backendData = text => {
+export const backendData = async (text, perPage, page) => {
   const options = new URLSearchParams({
       key: API_KEY,
       q: text,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      per_page: perPage,
+      page: page,
   });
-
-  return fetch(`${BASE_URL}?${options}`).then(response => {
-      if (!response.ok) {
-          throw new Error(response.status);
-      }
-      return response.json();
-  });
-};
+  return await axios.get(`${BASE_URL}?${options}`).then(response => response.data);
+}
